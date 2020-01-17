270096bd4d378df9773a0c091cead21fa034e71b
Ubuntu <sanzgiri@gmail.com>
2020-01-17 02:53:53 +0000

    cron job, cmd line interface

--- get_new_games.sh
+++ get_new_games.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-x=`cat $PWD/last_episode.txt`
+DIR="/home/ubuntu/jarchive"
+x=`cat $DIR/last_episode.txt`
 start=$(($x+1))
-/home/ubuntu/anaconda2/envs/jarchive/bin/python download.py $PWD/new_games 6507
-/home/ubuntu/anaconda2/envs/jarchive/bin/python parser.py -d $PWD/new_games > $PWD/jarchive_tmp.csv
-cat $PWD/jarchive_tmp.csv >> $PWD/jarchive.csv
-ls -1 $PWD/new_games/*.html | tail -n 1 | egrep -o '[0-9]+' > $PWD/last_episode.txt
-rm $PWD/new_games/*.html $PWD/jarchive_tmp.csv
+/home/ubuntu/anaconda2/envs/jarchive/bin/python $DIR/download.py $DIR/new_games $start
+/home/ubuntu/anaconda2/envs/jarchive/bin/python $DIR/parser.py -d $DIR/new_games > $DIR/jarchive_tmp.csv
+cat $DIR/jarchive_tmp.csv >> $DIR/jarchive.csv
+ls -1 $DIR/new_games/*.html | tail -n 1 | egrep -o '[0-9]+' > $DIR/last_episode.txt
+rm $DIR/new_games/*.html $DIR/jarchive_tmp.csv
