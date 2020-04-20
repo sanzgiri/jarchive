a9ff0a4a2f8e4d6685404313c6e3a3f0d470bea2
Ashutosh Sanzgiri <sanzgiri@gmail.com>
2020-04-20 00:04:13 -0700

    Create get_new_games.sh

---
+++ _action_files/get_new_games.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+x=`cat $PWD/last_episode.txt`
+start=$(($x+1))
+
+python3 $PWD/download.py $PWD/new_games $start
+python3 $PWD/parser.py -d $PWD/new_games > $PWD/jarchive_tmp.csv
+cat $PWD/jarchive_tmp.csv >> $PWD/jarchive.csv
+ls -1 $PWD/new_games/*.html | tail -n 1 | egrep -o '[0-9]+' > $PWD/last_episode.txt
+rm $PWD/new_games/*.html $PWD/jarchive_tmp.csv
